f38cbcec68e9f272eb76f89ca2e48d4fdf5c0d47
陳宜駿 <ichun.chen@kaust.edu.sa>
2025-09-15 16:27:09 +0800

    modify

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-#todo:請建立一個Flask應用程式，並在根路由('/')上設置一個視圖函數，該函數返回"Hello, World!"字串作為響應。
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def index():
+    return '<h1 style="color:red">Hello! Flask!</h1>'
+
+@app.route("/name")
+def name():
+    return '<h1>Hello! Route!</h1>'
+
+if __name__ == "__main__":
+    app.run(debug = True)
