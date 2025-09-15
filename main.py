cd95bc10ba0e4382cd8e4f51a2ea8b11f9696bfe
陳宜駿 <ichun.chen@kaust.edu.sa>
2025-09-15 15:44:27 +0800

    modify

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from python-web-2025!")
+#todo:請建立一個Flask應用程式，並在根路由('/')上設置一個視圖函數，該函數返回"Hello, World!"字串作為響應。
+from flask import Flask
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    main()
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+if __name__ == '__main__':
+    app.run(debug=True)
