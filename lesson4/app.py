275679dc30f062878643951e74b1f29c39f5b83a
陳宜駿 <ichun.chen@kaust.edu.sa>
2025-10-01 11:50:01 +0800

    modify

--- lesson4/app.py
+++ lesson4/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
-@app.route("/")
+@app.route("/") # "/" 代表根目錄，即為首頁。
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 def main():
     """啟動應用(教學用：啟用 debug 模式)"""
